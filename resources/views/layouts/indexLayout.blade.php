e6168bc5c046ffb8ec89b50551b2d6d99277a7e5
ultramenid <malichamdan@gmail.com>
2024-08-07 10:49:03 +0700

    edit analytic code

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -13,13 +13,16 @@
     @livewireScripts
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-9BQRCF1TCG"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GJ5GW51ZT"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-9BQRCF1TCG');
+    gtag('config', 'G-2GJ5GW51ZT');
     </script>
+
 
 </head>
 <body class="font-sans">
