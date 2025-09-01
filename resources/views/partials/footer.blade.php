f4ded74bf48311b828e9fb2b877c6ce8e55d030a
ultramenid <malichamdan@gmail.com>
2025-09-01 17:16:02 +0700

    logo footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,34 +5,34 @@
                     <h1>Co-creators</h1>
                     <div class="flex justify-between w-full flex-wrap">
                         <a href="https://auriga.or.id/" target="_blank" >
-                            <img class="h-10 mt-2" src="https://mapbiomas.nusantara.earth/assets/images/AurigaPNG.png" alt="">
+                            <img class="h-10 mt-2" src="https://mapbiomas.id/assets/logos/AurigaPNG.png" alt="">
                         </a>
                         <a href="https://www.jeratpapua.org/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo2_color.png" alt="" class="h-12 ">
+                            <img src="https://mapbiomas.id/assets/logos/logo2_color.png" alt="" class="h-12 ">
                         </a>
                         <a href="https://saveourborneo.org/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo3_color.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo3_color.png" alt="" class="h-12">
                         </a>
                         <a href="http://greenofborneo.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo4_color.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo4_color.png" alt="" class="h-12">
                         </a>
                         <a href="https://www.haka.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo5-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo5-x.png" alt="" class="h-12">
                         </a>
                         <a href="http://mnukwarpapua.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo6-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo6-x.png" alt="" class="h-12">
                         </a>
                         <a href="https://hutaninstitute.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo7-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo7-x.png" alt="" class="h-12">
                         </a>
                         <a href="http://yayasangenesisbengkulu.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo8-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo8-x.png" alt="" class="h-12">
                         </a>
                          <a href="https://komiu.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo9-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo9-x.png" alt="" class="h-12">
                         </a>
                         <a href="http://sampankalimantan.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo10-x.png" alt="" class="h-12">
+                            <img src="https://mapbiomas.id/assets/logos/logo10-x.png" alt="" class="h-12">
                         </a>
 
                     </div>
@@ -42,7 +42,7 @@
                 <div class="sm:w-2/12 w-full">
                     <h1 class="">Supported by</h1>
                     <a href="https://woods-wayside.org/" target="_blank" class="">
-                        <img src="https://mapbiomas.nusantara.earth/assets/images/logo12-x.png" alt="" class="h-12">
+                        <img src="https://mapbiomas.id/assets/logos/logo12-x.png" alt="" class="h-12">
                     </a>
                 </div>
 
