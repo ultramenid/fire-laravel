2f386cc2bd3e574b5fbedf88b3707ed2b45b5fb5
ultramenid <malichamdan@gmail.com>
2025-12-16 22:13:14 +0700

    navPc

--- resources/views/partials/navPC.blade.php
+++ resources/views/partials/navPC.blade.php
@@ -47,6 +47,20 @@
         </div>
 
         <a href="{{ route('newsnevent', [app()->getLocale()]) }}" class="text-red-600 cursor-pointer">news & event</a>
-        <a href="{{ route('downloads', [app()->getLocale()]) }}" class="text-red-600 cursor-pointer">downloads</a>
+
+        <!-- methodology dropdown -->
+        <div class="flex-col flex" x-data="{ pagesMethodology: false }">
+            <a @click="pagesMethodology = !pagesMethodology" @click.away="pagesMethodology = false" class="text-red-600 cursor-pointer inline-flex items-center">
+                downloads
+                <svg xmlns="http://www.w3.org/2000/svg" :class="{'rotate-180': pagesMethodology, 'rotate-0': !pagesMethodology}" class="w-4 ml-1 -mb-1 transition-transform duration-200 transform" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </a>
+            <div class="absolute mt-8 z-20 bg-white px-2 py-2 flex flex-col space-y-2 w-52 border-red-600 border-b" x-show="pagesMethodology" x-cloak>
+                <a href="{{ route('downloads', [app()->getLocale()]) }}" class="text-sm mr-6">collection map</a>
+                <a href="{{ route('infographics', [app()->getLocale()]) }}" class="text-sm mr-6">infographics</a>
+            </div>
+        </div>
+
     </div>
 </div>
